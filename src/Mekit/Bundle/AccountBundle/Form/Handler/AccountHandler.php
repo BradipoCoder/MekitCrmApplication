b3fe0e131d3b574e60190c600ad1a196e72ad064
adamjakab <me@jakabadambalazs.com>
2014-11-04 18:24:36 +0100

    added account form

---
+++ src/Mekit/Bundle/AccountBundle/Form/Handler/AccountHandler.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Mekit\Bundle\AccountBundle\Form\Handler;
+
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use Mekit\Bundle\AccountBundle\Entity\Account;
+//use Mekit\Bundle\ContactBundle\Entity\Contact;
+
+use Oro\Bundle\TagBundle\Entity\TagManager;
+use Oro\Bundle\TagBundle\Form\Handler\TagHandlerInterface;
+
+/**
+ * Class AccountHandler
+ * @package Mekit\Bundle\AccountBundle\Form\Handler
+ */
+class AccountHandler implements TagHandlerInterface {
+	/**
+	 * @var FormInterface
+	 */
+	protected $form;
+
+	/**
+	 * @var Request
+	 */
+	protected $request;
+
+	/**
+	 * @var ObjectManager
+	 */
+	protected $manager;
+
+	/**
+	 * @var TagManager
+	 */
+	protected $tagManager;
+
+	/**
+	 *
+	 * @param FormInterface $form
+	 * @param Request       $request
+	 * @param ObjectManager $manager
+	 */
+	public function __construct(FormInterface $form, Request $request, ObjectManager $manager) {
+		$this->form = $form;
+		$this->request = $request;
+		$this->manager = $manager;
+	}
+
+	/**
+	 * Process form
+	 *
+	 * @param  Account $entity
+	 * @return bool True on successful processing, false otherwise
+	 */
+	public function process(Account $entity) {
+		$this->form->setData($entity);
+		if (in_array($this->request->getMethod(), array('POST', 'PUT'))) {
+			$this->form->submit($this->request);
+			if ($this->form->isValid()) {
+				$this->onSuccess($entity);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * "Success" form handler
+	 *
+	 * @param Account $entity
+	 */
+	protected function onSuccess(Account $entity) {
+		$this->manager->persist($entity);
+		$this->manager->flush();
+		$this->tagManager->saveTagging($entity);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function setTagManager(TagManager $tagManager) {
+		$this->tagManager = $tagManager;
+	}
+}
---
+++ src/Mekit/Bundle/AccountBundle/Form/Type/AccountType.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Mekit\Bundle\AccountBundle\Form\Type;
+
+use Doctrine\Common\Collections\Collection;
+
+use Symfony\Component\Routing\Router;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+
+use Oro\Bundle\LocaleBundle\Formatter\NameFormatter;
+use Oro\Bundle\SecurityBundle\SecurityFacade;
+
+use Mekit\Bundle\AccountBundle\Entity\Account;
+
+//use Mekit\Bundle\ContactBundle\Entity\Contact;
+
+/**
+ * Class AccountType
+ * @package Mekit\Bundle\AccountBundle\Form\Type
+ */
+class AccountType extends AbstractType {
+
+	/**
+	 * @var Router
+	 */
+	protected $router;
+
+	/**
+	 * @var NameFormatter
+	 */
+	protected $nameFormatter;
+
+	/**
+	 * @var SecurityFacade
+	 */
+	protected $securityFacade;
+
+	/**
+	 * @var boolean
+	 */
+	private $canViewContact;
+
+	/**
+	 * @param Router         $router
+	 * @param NameFormatter  $nameFormatter
+	 * @param SecurityFacade $securityFacade
+	 */
+	public function __construct(Router $router, NameFormatter $nameFormatter, SecurityFacade $securityFacade) {
+		$this->nameFormatter = $nameFormatter;
+		$this->router = $router;
+		$this->securityFacade = $securityFacade;
+		$this->canViewContact = $this->securityFacade->isGranted('mekit_contact_view');
+	}
+
+	/**
+	 * @param FormBuilderInterface $builder
+	 * @param array                $options
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options) {
+		$builder
+			->add('name', 'text', ['required' => true, 'label' => 'mekit.account.name.label'])
+			->add('vatid', 'text', ['required' => false, 'label' => 'mekit.account.vatid.label'])
+			->add('nin', 'text', ['required' => false, 'label' => 'mekit.account.nin.label'])
+			->add('fax', 'text', ['required' => false, 'label' => 'mekit.account.fax.label'])
+			->add('website', 'text', ['required' => false, 'label' => 'mekit.account.website.label'])
+			->add('description', 'text', ['required' => false, 'label' => 'mekit.account.description.label'])
+
+            ->add('tags', 'oro_tag_select', ['label' => 'oro.tag.entity_plural_label']);
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function setDefaultOptions(OptionsResolverInterface $resolver) {
+		$resolver->setDefaults(
+			array(
+				'data_class' => 'Mekit\Bundle\AccountBundle\Entity\Account',
+				'intention' => 'account',
+				'extra_fields_message' => 'This form should not contain extra fields: "{{ extra_fields }}"',
+				'cascade_validation' => true
+			)
+		);
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName() {
+		return 'mekit_account';
+	}
+}
