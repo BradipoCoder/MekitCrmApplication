ad2302e968fc80f7eaa58adebd25342a30b81317
adamjakab <jakabadambalazs@gmail.com>
2015-02-14 19:07:14 +0100

    travis/scrutinizer testing

--- src/Mekit/Bundle/TestBundle/CI/Travis/before_script.sh
+++ src/Mekit/Bundle/TestBundle/CI/Travis/before_script.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# This configuration script is executed from project root so references to files
+# must also be from project root
+
+echo "Configuring php MySql..."
+ls -la /etc/mysql/conf.d
+/usr/sbin/mysqld --verbose --help | grep -A 1 "Default options"
+
 
 echo "Configuring php CLI..."
 phpenv config-add src/Mekit/Bundle/TestBundle/CI/Travis/travis.php.ini
