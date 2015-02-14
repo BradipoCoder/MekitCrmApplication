1b9a8d61c9c90a3457c91201323e65deb4cf1ab3
adamjakab <jakabadambalazs@gmail.com>
2015-02-14 22:17:56 +0100

    travis/scrutinizer testing - fixing mysql #2

--- src/Mekit/Bundle/TestBundle/CI/Travis/before_script.sh
+++ src/Mekit/Bundle/TestBundle/CI/Travis/before_script.sh
@@ -6,15 +6,17 @@
 echo "------------------------------------------------------------"
 echo "Configuring php MySql..."
 mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"
-cp src/Mekit/Bundle/TestBundle/CI/Travis/.my.cnf ~/
+cat src/Mekit/Bundle/TestBundle/CI/Travis/.my.cnf >> /usr/etc/my.cnf
+echo "------------------------------------------------------------"
+cat /usr/etc/my.cnf
+echo "------------------------------------------------------------"
 sudo service mysql restart
 mysql -e "SET GLOBAL wait_timeout=300;"
 mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"
 
-
+# Php
 echo "------------------------------------------------------------"
 echo "Configuring php CLI..."
 phpenv config-add src/Mekit/Bundle/TestBundle/CI/Travis/travis.php.ini
 
-echo "------------------------------------------------------------"
-php -i
+
