43e3e8711712842db3e95cc045a463926a962267
adamjakab <jakabadambalazs@gmail.com>
2015-02-14 19:31:52 +0100

    travis/scrutinizer testing - fixing mysql #2

--- src/Mekit/Bundle/TestBundle/CI/Travis/before_script.sh
+++ src/Mekit/Bundle/TestBundle/CI/Travis/before_script.sh
@@ -2,13 +2,17 @@
 # This configuration script is executed from project root so references to files
 # must also be from project root
 
+# MySql - fix "Mysql has gone away" problem
+echo "------------------------------------------------------------"
 echo "Configuring php MySql..."
 mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"
 cp src/Mekit/Bundle/TestBundle/CI/Travis/.my.cnf ~/
 sudo service mysql restart
 mysql -e "SET GLOBAL wait_timeout=300;"
+mysql -e "SHOW VARIABLES LIKE 'max_allowed_packet';"
 
 
+echo "------------------------------------------------------------"
 echo "Configuring php CLI..."
 phpenv config-add src/Mekit/Bundle/TestBundle/CI/Travis/travis.php.ini
 
