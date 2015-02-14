26b75e5ac0fb0134187e8239c5d2fb78aa03308e
adamjakab <jakabadambalazs@gmail.com>
2015-02-14 23:51:06 +0100

    travis/scrutinizer testing #6

--- src/Mekit/Bundle/AccountBundle/Tests/Functional/AccountControllerTest.php
+++ src/Mekit/Bundle/AccountBundle/Tests/Functional/AccountControllerTest.php
@@ -2,6 +2,7 @@
 namespace Mekit\Bundle\AccountBundle\Tests\Functional;
 
 use Mekit\Bundle\TestBundle\Helpers\MekitFunctionalTest;
+use Symfony\Component\DomCrawler\Form;
 
 
 class AccountControllerTest extends MekitFunctionalTest {
@@ -15,4 +16,18 @@ class AccountControllerTest extends MekitFunctionalTest {
 		$result = $this->client->getResponse();
 		$this->assertTrue($result->isOk());
 	}
+
+    public function testCreateAction() {
+        $crawler = $this->client->request('GET', $this->getUrl('mekit_account_create'));
+        /** @var Form $form */
+        $form = $crawler->selectButton('Save and Close')->form();
+        $form['mekit_account_form[name]'] = 'Mekit';
+        $this->client->followRedirects(true);
+        $crawler = $this->client->submit($form);
+        $result = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($result, 200);
+        //check if we are on view page
+
+    }
+
 }
