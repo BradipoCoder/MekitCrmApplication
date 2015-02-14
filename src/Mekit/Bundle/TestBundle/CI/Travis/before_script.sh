ac572f020b7b5a83ae0676b51f6161845fb26b5c
adamjakab <jakabadambalazs@gmail.com>
2015-02-14 18:48:19 +0100

    travis/scrutinizer testing

--- src/Mekit/Bundle/TestBundle/CI/Travis/before_script.sh
+++ src/Mekit/Bundle/TestBundle/CI/Travis/before_script.sh
@@ -1,10 +1,7 @@
-#!/bin/sh
-
-echo "------------------------------------------------------------"
-php -i
+#!/bin/bash
 
 echo "Configuring php CLI..."
-phpenv config-add travis.php.ini
+phpenv config-add src/Mekit/Bundle/TestBundle/CI/Travis/travis.php.ini
 
 echo "------------------------------------------------------------"
 php -i
