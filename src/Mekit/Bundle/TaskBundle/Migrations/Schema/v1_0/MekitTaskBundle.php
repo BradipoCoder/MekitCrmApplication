0df01d2de5db1c839e10e7fb40e897788d83b10c
jakabadambalazs <jakabadambalazs@gmail.com>
2015-03-16 13:06:12 +0100

    taskBundle - added type in migration

--- src/Mekit/Bundle/TaskBundle/Migrations/Schema/v1_0/MekitTaskBundle.php
+++ src/Mekit/Bundle/TaskBundle/Migrations/Schema/v1_0/MekitTaskBundle.php
@@ -29,6 +29,7 @@ class MekitTaskBundle implements Migration {
 		$table->addColumn('id', 'integer', ['autoincrement' => true]);
 		$table->addColumn('event_id', 'integer', []);
 		$table->addColumn('name', 'string', ['length' => 255]);
+		$table->addColumn('type', 'integer', ['notnull' => false]);
 		$table->addColumn('project_id', 'integer', ['notnull' => false]);
 		$table->addColumn('owner_id', 'integer', ['notnull' => false]);
 		$table->addColumn('organization_id', 'integer', ['notnull' => false]);
@@ -38,6 +39,7 @@ class MekitTaskBundle implements Migration {
 		$table->addIndex(['owner_id'], 'idx_task_owner', []);
 		$table->addIndex(['organization_id'], 'idx_task_organization', []);
 		$table->addIndex(['name'], 'idx_task_name', []);
+		$table->addIndex(['type'], 'idx_task_type', []);
 		$table->addUniqueIndex(['event_id'], 'idx_task_event', []);
 		$table->addIndex(['project_id'], 'idx_task_project', []);
 
@@ -62,6 +64,13 @@ class MekitTaskBundle implements Migration {
 			['id'],
 			['onDelete' => 'CASCADE'],
 			'fk_task_event'
+		);
+		$table->addForeignKeyConstraint(
+			$schema->getTable('mekit_list_item'),
+			['type'],
+			['id'],
+			['onDelete' => null, 'onUpdate' => null],
+			'fk_task_type'
 		);
 		$table->addForeignKeyConstraint(
 			$schema->getTable('mekit_project'),
